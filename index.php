fc0e5f0565344c15d0f385ca6167b1ae219da6d1
chaidarsaad <chaidarsaad55@gmail.com>
2024-10-21 16:58:39 +0700

    update

--- index.php
+++ index.php
@@ -163,10 +163,10 @@
                         echo '<div>';
                         echo "<a href='http://localhost:8042/app/explorer.html#patient?uuid=$parentPatientUuid' target='_blank'>
                                 <button type='button'>Explorer</button>
-                            </a>";
+                              </a>";
                         echo "<a href='http://localhost:8042/volview/index.html?names=[archive.zip]&urls=[../studies/$studyID/archive]' target='_blank'>
-                <button type='button'>Volview</button>
-              </a>";
+                                <button type='button'>Volview</button>
+                              </a>";
                         echo '</div>';
                     }
                 } else {
